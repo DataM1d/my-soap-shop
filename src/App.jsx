cd5d857a9252b1b0531932882e29afe52f273f29
Data <d9044839@gmail.com>
2026-02-04 08:34:15 +0100

    feat: initial clean slate with Tailwind v4, TanStack Query, and Absolute Imports

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-soap-green-700 font-serif">
+      If this is big, bold, green, and serif, Tailwind is working!
+    </h1>
+  )
+}
